dfc030f584ac609147087cf09c43e1a833fb7774
Oraclesc <oraclesupportcentre@proton.me>
2026-09-04 12:06:42 +0100

    Bump service worker cache for Ask the Tarot journal fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="oracle-support-v17";
+const CACHE_NAME="oracle-support-v18";
 const CORE=["/","/index.html","/cards.html","/guides.html","/reading.html","/tools.html","/card-of-the-day.html","/learn-tarot.html","/tarot-journal.html","/tarot-combinations.html","/card-search.html","/favourites.html","/ask-the-tarot.html","/relationship-reading.html","/career-reading.html","/yes-no-reading.html","/my-tarot-journey.html","/css/style.css","/css/tools.css","/css/home-v2-final.css","/data/tarot-data.js","/Js/script.js","/Js/tools.js","/Js/daily.js","/Js/phase12.js","/Js/phase3.js","/Js/home-v2-final.js","/Js/home-journey.js","/Js/school-progress.js","/images/moon.webp","/images/card-back.webp"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
